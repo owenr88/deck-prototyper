e6989f2fcc3640bab34fb28d3784fa87813277a8
Owen Richards <owen@biglemon.co.uk>
2020-08-23 11:48:45 +0100

    Card index + 1 on random data

--- src/data/factories/cards.ts
+++ src/data/factories/cards.ts
@@ -4,14 +4,14 @@ import faker from 'faker';
 import { CardType, DeckType, CardDecks } from '../../types';
 
 export const makeCards = (total: number = 1, decks: DeckType[]): CardType[] => {
-  return times(total, (number) => {
+  return times(total, (i) => {
     let deckData: CardDecks = {};
     sampleSize(decks, random(0, decks.length)).forEach((d) => {
       deckData[d.number] = random(1, 2);
     });
     return {
-      number,
-      title: number + ': ' + faker.lorem.words(3),
+      number: i + 1,
+      title: i + ': ' + faker.lorem.words(3),
       body1: faker.lorem.sentence(),
       body2: faker.lorem.sentence(),
       decks: deckData,
